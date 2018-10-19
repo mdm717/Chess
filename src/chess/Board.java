b3710ca3e0c7ebf3397a4872d0e5ed64ae5b8582
Craig Sirota <craigmsirota@gmail.com>
2018-10-19 12:42:49 -0400

    Promotion has been implemented

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -7,27 +7,27 @@ public class Board {
 	
 	public Board () {
 		for(int i = 0; i < 8; i++) {
-//			board[1][i]=new Pawn(true);
-			board[1][i]=new Pawn(false);
+			board[1][i]=new Pawn(true);
+			board[6][i]=new Pawn(false);
 		}
 
-//		board[7][0]=new Rook(false);
-//		board[7][1]=new Knight(false);
-//		board[7][2]=new Bishop(false);
-//		board[7][3]=new Queen(false);
-//		board[7][4]=new King(false);
-//		board[7][5]=new Bishop(false);
-//		board[7][6]=new Knight(false);
-//		board[7][7]=new Rook(false);
+		board[7][0]=new Rook(false);
+		board[7][1]=new Knight(false);
+		board[7][2]=new Bishop(false);
+		board[7][3]=new Queen(false);
+		board[7][4]=new King(false);
+		board[7][5]=new Bishop(false);
+		board[7][6]=new Knight(false);
+		board[7][7]=new Rook(false);
 		
-//		board[0][0]=new Rook(true);
+		board[0][0]=new Rook(true);
 		board[0][1]=new Knight(true);
-//		board[0][2]=new Bishop(true);
-//		board[0][3]=new Queen(true);
-//		board[0][4]=new King(true);
-//		board[0][5]=new Bishop(true);
-//		board[0][6]=new Knight(true);
-//		board[0][7]=new Rook(true);
+		board[0][2]=new Bishop(true);
+		board[0][3]=new Queen(true);
+		board[0][4]=new King(true);
+		board[0][5]=new Bishop(true);
+		board[0][6]=new Knight(true);
+		board[0][7]=new Rook(true);
 		
 		
 		
